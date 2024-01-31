9cf6a379eb564f6fc7d93b85d3da3552114b18c7
Lam Nguyen <nguyenthanhlam091@gmail.com>
2024-01-31 16:51:33 +1100

    todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [job, setJob]= useState('')
+  const [jobs, setJobs] = useState([])
+
+  const handleAdd = () => {
+    setJobs((prev) => [...prev, job])
+    setJob('')
+  }
+
+  const handleRemove = (index) => {
+    setJobs((prev) => {
+      const updatedJob = [...prev]; 
+      updatedJob.splice(index, 1);
+
+      return updatedJob;
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: 30}} className="todoList">
+      <input type='text' value={job} onChange={(e) => setJob(e.target.value)} placeholder='Enter job'></input>
+      <button onClick={handleAdd}>Add Job</button>
+
+      <ul >
+        {console.log(jobs)}
+        {jobs.map((job, index) => (
+          <li key={index}>
+            {job}
+            <button onClick={()=>handleRemove(index)}>Remove</button>
+
+          </li>
+        ))}
+      </ul>
+
     </div>
   );
 }
